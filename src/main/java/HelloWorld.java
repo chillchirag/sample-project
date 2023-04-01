30d474df245c51da58b9774246a3e3d257b89567
Chirag Gupta <chirag.gupta@go-mmt.com>
2023-04-01 18:40:20 +0530

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        System.out.println("Hello World");
+        System.out.println(c);
+    }
+}
